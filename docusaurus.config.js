837ada9e1c5f046b49ab7a449bd4340283cf16ef
Bhardwaj-Himanshu <himanshujay261201@gmail.com>
2023-04-11 21:25:43 +0530

    this is another random message

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/pipeline/',
+  baseUrl: '/pipelines/',
   trailingSlash:true,
   deploymentBranch:'gh-pages',
 
